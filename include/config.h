8dd8fa86b917c981817b561b67eece4412e09505
Cohen Mejan <cohenmejan@gmail.com>
2024-12-24 03:56:25 -0500

    add premake and config

---
+++ include/config.h
@@ -0,0 +1,3 @@
+#define FLAC__NO_DLL
+#define FLAC__NO_OGG
+#define FLAC__HAS_OGG 0
---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "FLAC"
+    kind "StaticLib"
+    language "C++"
+	systemversion "latest"
+	staticruntime "on"
+	
+	targetdir "%{wks.location}/bin/%{cfg.buildcfg}/%{prj.name}"
+	objdir "%{wks.location}/bin-obj/%{cfg.buildcfg}/%{prj.name}"
+
+	flags {
+		"MultiProcessorCompile",
+	}
+
+    includedirs {
+        "include",
+		"src/libFLAC/include"
+    }
+
+    files {
+		"include/*.h",
+		"include/FLAC/*.h",
+		"include/FLAC++/*.h",
+		"src/libFLAC/*.c",
+		"src/libFLAC++/*.cpp",
+		"src/share/win_utf8_io/*.c",
+    }
+
+	removefiles {
+		"src/libFLAC/ogg_*.c"
+	}
+
+    defines {
+		"HAVE_CONFIG_H",
+    	"PACKAGE_VERSION=\"1.3.4\"",
+		"_CRT_SECURE_NO_DEPRECATE",
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
